d0c39498ff7d55ec02e0ce53ef5138ffc0f02890
K1R1LL117 <n17k17@yandex.ru>
2025-09-11 18:51:47 +0300

    fix redis: add close conn, del debug prints

--- src/services/genre.py
+++ src/services/genre.py
@@ -12,15 +12,12 @@ class GenreService:
         self.genre_repository = GenreRepository(session)
 
     async def get_all_genres(self):
-        print("Вызов функции для получения всех жанров")
-        redis_client = redis.Redis(host=settings.redis.HOST)
-        cache_genres = redis_client.get("book_genres")
+        with redis.Redis(host=settings.redis.HOST) as redis_client:
+            cache_genres = redis_client.get("book_genres")
 
-        if cache_genres is not None:
-            print("Отдайм данные из редиски")
-            return pickle.loads(cache_genres)
-        
-        result = await self.genre_repository.get_all()
-        redis_client.set("book_genres", pickle.dumps(result), ex=1800)
-        print("Отдаём данные из БД")
-        return result
+            if cache_genres is not None:
+                return pickle.loads(cache_genres)
+            
+            result = await self.genre_repository.get_all()
+            redis_client.set("book_genres", pickle.dumps(result), ex=1800)
+            return result
